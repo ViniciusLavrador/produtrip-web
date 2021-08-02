66457082b07370f081923dcc48581ad6ae26dffe
Vinícius Lavrador <vinicius.lavrador@icloud.com>
2021-08-02 14:59:35 -0300

    Removed Buggy Chat Component

--- components/Chat/Message.tsx
+++
@@ -1,8 +0,0 @@
-export interface MessageProps {
-  content: string;
-  user?: any;
-  sent?: boolean;
-  timestamp: Date;
-}
-
-const SentMessage = ({ content, user, timestamp, sent }: MessageProps) => {};
